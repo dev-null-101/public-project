ac02f34ba8be0d58f78a634f743041cc607484c3
Null <devnull0110@proton.me>
2024-04-01 18:18:56 +0800

    rg

--- terraform/azure/resource_group/variables.tf
+++ terraform/azure/resource_group/variables.tf
@@ -12,6 +12,7 @@ variable "secret" {
   description = "The username for the DB master user"
   type        = string
 }
+
 variable "tenantid" {
   description = "The password for the DB master user"
   type        = string
