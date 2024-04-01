0e049233e6f79c278f5ce9cba7bd8a807100a1ec
Null <devnull0110@proton.me>
2024-04-01 18:48:52 +0800

    rg

--- terraform/azure/resource_group/variables.tf
+++ terraform/azure/resource_group/variables.tf
@@ -1,19 +1,15 @@
 variable "appid" {
-  description = "The username for the DB master user"
   type        = string
 }
 
 variable "subscriptionid" {
-  description = "The password for the DB master user"
   type        = string
 }
 
 variable "secret" {
-  description = "The username for the DB master user"
   type        = string
 }
 
 variable "tenantid" {
-  description = "The password for the DB master user"
   type        = string
 }
