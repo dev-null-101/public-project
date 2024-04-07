b8ccf59a428583efdec813428238d02a06ab5f54
Null <devnull0110@proton.me>
2024-04-07 09:26:17 +0800

    terra aks

---
+++ terraform/azure/aks/locals.tf
@@ -0,0 +1,8 @@
+locals {
+    env                 = "dev"
+    region              = "southeastasia"
+    resource_group_name = "rg-aks-dev-01"
+    aks_name            = "aks-dev-01"
+    aks_version         = "1.27"
+}
+
