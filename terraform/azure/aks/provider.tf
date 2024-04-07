d12b98821d9457e0a32f3a5fff386249273b1480
Null <devnull0110@proton.me>
2024-04-07 09:32:46 +0800

    aks

--- terraform/azure/aks/provider.tf
+++ terraform/azure/aks/provider.tf
@@ -1,5 +1,9 @@
 provider "azurerm" {
     features {}
+    subscription_id = var.subscriptionid
+    client_id       = var.appid
+    client_secret   = var.secret
+    tenant_id       = var.tenantid
 }
 
 terraform {
