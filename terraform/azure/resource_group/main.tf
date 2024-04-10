65233668098a29256d7dd731c9395eec0363e5cc
Null <devnull0110@proton.me>
2024-04-10 12:00:20 +0800

    rg

--- terraform/azure/resource_group/main.tf
+++ terraform/azure/resource_group/main.tf
@@ -1,12 +1,9 @@
-# provider "azurerm" {
-#   features {}
-#   subscription_id = var.subscriptionid
-#   client_id       = var.appid
-#   client_secret   = var.secret
-#   tenant_id       = var.tenantid
-# }
-module "azure_provider" {
-  source = "./modules/azure_provider"
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscriptionid
+  client_id       = var.appid
+  client_secret   = var.secret
+  tenant_id       = var.tenantid
 }
 
 resource "azurerm_resource_group" "example" {
--- terraform/azure/resource_group/variables.tf
+++ terraform/azure/resource_group/variables.tf
@@ -1,19 +1,19 @@
-# variable "appid" {
-#   type        = string
-#   sensitive   = true
-# }
+variable "appid" {
+  type        = string
+  sensitive   = true
+}
 
-# variable "subscriptionid" {
-#   type        = string
-#   sensitive   = true
-# }
+variable "subscriptionid" {
+  type        = string
+  sensitive   = true
+}
 
-# variable "secret" {
-#   type        = string
-#   sensitive   = true
-# }
+variable "secret" {
+  type        = string
+  sensitive   = true
+}
 
-# variable "tenantid" {
-#   type        = string
-#   sensitive   = true
-# }
+variable "tenantid" {
+  type        = string
+  sensitive   = true
+}
