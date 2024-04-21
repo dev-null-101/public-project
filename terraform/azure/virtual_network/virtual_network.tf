a8ff0e06eaab35c3767a73ea6a3b602b392acaa1
Neil Flores <devnull0110@proton.me>
2024-04-21 08:58:37 +0800

    update

--- terraform/azure/virtual_network/virtual_network.tf
+++ terraform/azure/virtual_network/virtual_network.tf
@@ -2,11 +2,13 @@ provider "azurerm" {
   features {}
 }
 
+#	rg-<app or service name>-<subscription purpose>-<###>
 resource "azurerm_resource_group" "example" {
   name     = "example-resources"
   location = "East US"
 }
 
+# vnet-<subscription purpose>-<region>-<###>
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
   resource_group_name = azurerm_resource_group.example.name
@@ -14,6 +16,7 @@ resource "azurerm_virtual_network" "example" {
   address_space       = ["10.0.0.0/16"]
 }
 
+# snet-<subscription purpose>-<region>-<###>
 resource "azurerm_subnet" "example" {
   count                = 2
   name                 = "subnet-${count.index}"
@@ -22,6 +25,7 @@ resource "azurerm_subnet" "example" {
   address_prefixes     = ["10.0.${count.index}.0/24"]
 }
 
+# nsg-<policy name or app name>-<###>
 resource "azurerm_network_security_group" "example" {
   count               = 2
   name                = "example-nsg-${count.index}"
